db1d635bc3ced837c430605634277bca5e249bdc
Mike Brooks <PDXmjb@users.noreply.github.com>
2018-02-09 11:01:37 -0800

    Performs the QuickSort function. Heavily utilizes Lamda expressions.
    Performs a sort by recursively picking a pivot and moving values less than the pivot to one side, and greater than the pivot to another, then performing the operation on the smaller arrays also.

---
+++ QuickSort/QuickSorter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace QuickSort
+{
+    public class QuickSorter
+    {
+        public static void Main(string[] args)
+        {
+            var input = new int[] { 6, -5, 23, 3, 18000, 4, -12, 7 };
+            var output = QuickSort(input);
+            Console.WriteLine(String.Format("Input: [{0}]", string.Join(", ", input)));
+            Console.WriteLine(String.Format("Output: [{0}]", string.Join(", ", output)));
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Performs a QuickSort given an unsorted array of integers.
+        /// </summary>
+        /// <param name="array">An unsorted array of integers</param>
+        /// <returns>A sorted array of integers</returns>
+        public static int[] QuickSort(int[] array)
+        {
+            // If there is only one thing, it's sorted!
+            if (array.Length <= 1)
+            {
+                return array;
+            }
+            // Otherwise,
+            else
+            {
+                // Choose the last number in the array as our pivot.
+                var pivot = new int[] { array.Last() };
+
+                // Recurse on everything lower than the pivot,
+                var sortedLower = QuickSort(array.Where(x => x < pivot[0]).ToArray<int>());
+
+                // Recurse on everything higher than the pivot,
+                var sortedHigher = QuickSort(array.Where(x => x > pivot[0]).ToArray<int>());
+
+                // Put them together!
+                return sortedLower.Concat(pivot.Concat(sortedHigher)).ToArray();
+            }
+        }
+    }
+}
---
+++ QuickSort/QuickSorterTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using QuickSort;
+
+[TestFixture]
+public class QuickSorterTests
+{
+    [TestCase(new int[] { 4, 5, 6 }, new int[] { 4, 5, 6 }, TestName = "In Order")]
+    [TestCase(new int[] {}, new int[] {}, TestName = "Empty")]
+    [TestCase(new int[] { 3, 1, 5, 2, 8 }, new int[] { 1, 2, 3, 5, 8 }, TestName = "Out of order (OOO)")]
+    [TestCase(new int[] { 6, -5, 2, 1, 18, -23, 45 }, new int[] { -23, -5, 1, 2, 6, 18, 45 }, TestName = "Negative OOO")]
+    public void TestInOrder(int[] value, int[] expected)
+    {
+        Assert.AreEqual(expected, QuickSorter.QuickSort(value));
+    }    
+}
